643006ea2550f4f408951fbc0fc4ef4e03d4f9f4
Jon Crain <joncrain@gmail.com>
2018-12-12 08:37:17 -0500

    First commit

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# osquery controller
+CTL="${BASEURL}index.php?/module/osquery/"
+
+# Get the scripts in the proper directories
+"${CURL[@]}" "${CTL}get_script/osqueryd" -o "${MUNKIPATH}osqueryd"
+"${CURL[@]}" "${CTL}get_script/osqueryi" -o "${MUNKIPATH}osqueryi"
+
+# Check exit status of curl
+if [ $? = 0 ]; then
+	# Make executable
+	chmod a+x "${MUNKIPATH}osqueryd"
+	chmod a+x "${MUNKIPATH}osqueryi"
+
+else
+	echo "Failed to download all required components!"
+	rm -f "${MUNKIPATH}osquery*"
+
+	# Signal that we had an error
+	ERR=1
+fi
---
+++ scripts/uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Remove osquery scripts
+rm -f "${MUNKIPATH}osquery*"
